b0b8dae7a1b58a36eaeee0b5c4026eb5c7e43999
Sasa <sasa.zelic@outlook.com>
2023-05-27 09:01:31 -0700

    sasa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">Sasa</router-link>
+    <router-link to="/about">Abboutt</router-link>
   </nav>
   <router-view/>
 </template>
